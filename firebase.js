ac99f52f27fca47762504e0730d87b5cc2dcfae9
Deethya Reddy Yelamkonda <deethyareddy.y@gmail.com>
2024-08-07 13:04:46 -0400

    first commit

--- firebase.js
+++ firebase.js
@@ -1,18 +1,20 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+import {initializeApp} from "firebase/app";
+import {getFirestore} from "firebase/firestore"
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID
+  apiKey: "AIzaSyBdrj4h3o8ugpWUcEVyzitIphu5DkpYcsw",
+  authDomain: "pantryapp-1fdce.firebaseapp.com",
+  projectId: "pantryapp-1fdce",
+  storageBucket: "pantryapp-1fdce.appspot.com",
+  messagingSenderId: "960542787782",
+  appId: "1:960542787782:web:752253c6ba79ed9d2eafc7"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const firestore = getFirestore(app);
-export { firestore };
+const firestore = getFirestore(app)
+export{firestore}
